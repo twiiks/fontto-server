a635e2b77ae4bdd1f6a88da6f6c719d244dbddec
sungjunyoung <wnsdud1861@gmail.com>
2017-12-07 00:56:56 +0900

    마이페이지

--- imports/ui/page/MyPage.jsx
+++ imports/ui/page/MyPage.jsx
@@ -7,6 +7,7 @@ import 'react-s-alert/dist/s-alert-default.css';
 import 'react-s-alert/dist/s-alert-css-effects/jelly.css';
 
 import '../style/page/MyPage.scss'
+import {Card, CardActions, CardHeader, CardMedia, CardText, CardTitle, FlatButton} from "material-ui";
 
 
 export class MyPage extends Component {
@@ -43,7 +44,7 @@ export class MyPage extends Component {
         this.setState({width: window.innerWidth, height: window.innerHeight});
     }
 
-    onLogout(){
+    onLogout() {
         Meteor.logout();
         location.href = '/';
     }
@@ -68,7 +69,53 @@ export class MyPage extends Component {
                             </div>
 
                         }/>
-                <Alert effect='jelly' stack={{limit: 3}}/>
+                <div className='mypage-content'>
+                    <div className='myfont'>내 폰트</div>
+
+                    <div className='font-card'>
+                        <div className='font-title'>
+                            폰토체
+                        </div>
+                        <div className='font-delete'>
+                            ×
+                        </div>
+                        <div className='divider'></div>
+                        <div className='font-desc'>
+                            폰토가 만든 첫번째 폰트! 폰토체입니다. :)
+                        </div>
+
+                        <div className='buttons-wrapper'>
+                            <div className='woff-button'>
+                                CSS 태그 복사하기
+                            </div>
+                            <div className='ttf-button'>
+                                TTF 다운받기
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='font-card'>
+                        <div className='font-title'>
+                            준영체
+                        </div>
+                        <div className='font-delete'>
+                            ×
+                        </div>
+                        <div className='divider'></div>
+                        <div className='font-desc'>
+                            한글자한글자 정성을 다해 만들었습니다.
+                        </div>
+
+                        <div className='buttons-wrapper'>
+                            <div className='woff-button'>
+                                CSS 태그 복사하기
+                            </div>
+                            <div className='ttf-button'>
+                                TTF 다운받기
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
