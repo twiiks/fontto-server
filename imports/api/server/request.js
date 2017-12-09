1253abf6d3eec40609d9326e725386f4254aa1bd
sungjunyoung <wnsdud1861@gmail.com>
2017-12-09 23:33:22 +0900

    ip 변경

--- imports/api/server/request.js
+++ imports/api/server/request.js
@@ -20,7 +20,7 @@ Meteor.methods({
         const f = new Future();
         console.log(unicodes);
         request.post({
-                url: 'http://13.125.77.172:5000',
+                url: 'http://34.211.190.122:5000',
                 json: {
                     userId: Meteor.userId(),
                     unicodes: unicodes,
